eadb1dfb1ebfb46c9e2ee704bb9c3a9bf585464c
PastelProgrammer <63501332+PastelProgrammer@users.noreply.github.com>
2021-03-26 17:40:11 -0700

    Add files via upload

---
+++ SolsFringeReborn.cs
@@ -0,0 +1,21 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace SolsFringeReborn
+{
+	public class SolsFringeReborn : Mod
+	{
+	
+                   public override void AddRecipes()
+  {                               
+  ModRecipe recipe = new ModRecipe(this);          
+  recipe.AddIngredient(this, "TrueSolsFringe");    
+  recipe.AddIngredient(ItemID.TrueExcalibur, 1);  
+  recipe.AddIngredient(ItemID.BrokenHeroSword, 1);
+  recipe.AddTile(134);      
+  recipe.SetResult(ItemID.TerraBlade);
+  recipe.AddRecipe();
+  		}
+  	}
+  }
